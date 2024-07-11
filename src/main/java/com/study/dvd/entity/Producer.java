43122b5e91e8fa6815150acb9f0bc4a439f87ece
ParkKKU <pku02089@gmail.com>
2024-07-11 17:43:54 +0900

    240711

---
+++ src/main/java/com/study/dvd/entity/Producer.java
@@ -0,0 +1,13 @@
+package com.study.dvd.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class Producer {
+	
+	private int producerId;
+	private String producerName;
+
+}
